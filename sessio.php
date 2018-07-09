3da2873e4380bec27245aa285fa48db09a941254
AdrigomRoc <adrigomroc@gmail.com>
2018-07-09 09:40:07 +0200

    base

---
+++ sessio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+?>
+<form action="sessio.php" method="post">
+    Usuari:
+    <input type="text" name="user">
+    <br/>
+    Contrasenya:
+    <input type="password" name="password">
+    <br />
+    <input type="submit">
+</form>
+</body>
+</html>
